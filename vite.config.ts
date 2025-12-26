77ec1beb8b2c27882c4e865d73d96ee001f485b9
Rubens Mariuzzo <rubens@mariuzzo.com>
2025-12-26 15:59:38 -0400

    Remove nitro dependency and plugin from package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   base: '/tools/',
   plugins: [
     devtools(),
-    nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
